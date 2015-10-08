4d5abc9a238fd699349f20e94d41111a75b8b134
scarecroweib <scarecroweib@gmail.com>
2015-10-08 16:04:47 +0800

    公司上传测试

---
+++ src/helloworld/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	str1 := "Hello Go World"
+	fmt.Println(str1)
+
+	i32_1 := 1
+	i32_2 := 2
+	i32_3 := 3
+	i32_4 := 4
+
+	fmt.Printf("%d,%d,%d,%d\n", i32_1, i32_2, i32_3, i32_4)
+}
